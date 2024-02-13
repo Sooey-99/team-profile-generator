afba3caefacad69e1d37e8bbeb940072e011cd08
Émile Siou <104312278+Sooey-99@users.noreply.github.com>
2024-02-13 22:18:17 +0000

    added html

--- starter/index.js
+++ starter/index.js
@@ -11,15 +11,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
-
-// 1. nmp init -Y ✅
-// 2. `npm i -D  jest` ✅
-// 3. Inquire ✅
-// 4. Html file creation (same as last week assignment)
-// 5. Create classes 1. parent class with all qualities. 2. create 3 classes with varying properties i.e. email, name, id number, etc
-// Build static website with cards and run on live server
+// Code to gather information about the development team members, and render the HTML file.
 
 function init() {
     inquirer.prompt([
--- starter/lib/Manager.js
+++ starter/lib/Manager.js
@@ -1,5 +1,4 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
-const Employee = require("./Employee");
+// Code to define and export the Manager class.
 
 class Manager extends Employee {
     constructor(id, name, email, officeNumber) {
