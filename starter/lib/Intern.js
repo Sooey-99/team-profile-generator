e2e272345c599eebbeb566f954523d2aea1d6ef9
Émile Siou <104312278+Sooey-99@users.noreply.github.com>
2024-02-13 21:58:13 +0000

    adding intern js

--- starter/lib/Intern.js
+++ starter/lib/Intern.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Intern extends Employee {
+  constructor(id, name, email, school) {
+    super(id, name, email);
+    this.school = school;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getSchool() {
+    return this.school;
+  }
+
+  getRole() {
+    return "Intern";
+  }
+  getName() {
+    return this.name;
+  }
+}
+
+module.exports = Intern;
